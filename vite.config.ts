759517442347746dc0fb26d6d4d030f6cef2615a
Aylo Srd <aylo.srd@gmail.com>
2025-08-06 09:50:04 +0200

    Add dts for type creation

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ include: ["lib"] })],
   build: {
     lib: {
-      name: 'react-pagination-hook',
-      entry: ['lib/index.ts'],
-      fileName: (format, entryName) => `react-pagination-hook-${entryName}.${format}.js`,
+      name: "react-pagination-hook",
+      entry: ["lib/index.ts"],
+      fileName: (format, entryName) =>
+        `react-pagination-hook-${entryName}.${format}.js`,
     },
   },
-})
+});
